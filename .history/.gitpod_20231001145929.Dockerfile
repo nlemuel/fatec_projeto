1f9d4c57bf27b3e40a87ac2b7d603bc0a06fb748
nlemuel <lemuelayres@gmail.com>
2023-10-01 15:12:47 -0300

    inicial commit

---
+++ .history/.gitpod_20231001145929.Dockerfile
@@ -0,0 +1,9 @@
+FROM gitpod/workspace-mysql
+
+USER gitpod 
+
+RUN /bin/bash -c ". /home/gitpod/.sdkman/bin/sdkman-init-sh \
+        && sdk install java 17.0.9-oracle < /dev/null \
+        && sdk flush archives \
+        && sdk flush temp
+"
---
+++ src/test/java/com/fatec/fatec_projeto/FatecProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.fatec_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatecProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
